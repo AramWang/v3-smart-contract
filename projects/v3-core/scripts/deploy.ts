538ce511946e8b9fa0e202dd764a2b35a41042cf
dqtkien <dqtkien@gmail.com>
2023-05-31 23:31:28 +0700

    chore: Update beraV3Pool

--- projects/v3-core/scripts/deploy.ts
+++ projects/v3-core/scripts/deploy.ts
@@ -6,9 +6,9 @@ import fs from 'fs'
 type ContractJson = { abi: any; bytecode: string }
 const artifacts: { [name: string]: ContractJson } = {
   // eslint-disable-next-line global-require
-  PancakeV3PoolDeployer: require('../artifacts/contracts/PancakeV3PoolDeployer.sol/PancakeV3PoolDeployer.json'),
+  BeraV3PoolDeployer: require('../artifacts/contracts/BeraV3PoolDeployer.sol/BeraV3PoolDeployer.json'),
   // eslint-disable-next-line global-require
-  PancakeV3Factory: require('../artifacts/contracts/PancakeV3Factory.sol/PancakeV3Factory.json'),
+  BeraV3Factory: require('../artifacts/contracts/BeraV3Factory.sol/BeraV3Factory.json'),
 }
 
 async function main() {
@@ -18,47 +18,38 @@ async function main() {
 
   let pancakeV3PoolDeployer_address = ''
   let pancakeV3PoolDeployer
-  const PancakeV3PoolDeployer = new ContractFactory(
-    artifacts.PancakeV3PoolDeployer.abi,
-    artifacts.PancakeV3PoolDeployer.bytecode,
+  const BeraV3PoolDeployer = new ContractFactory(
+    artifacts.BeraV3PoolDeployer.abi,
+    artifacts.BeraV3PoolDeployer.bytecode,
     owner
   )
   if (!pancakeV3PoolDeployer_address) {
-    pancakeV3PoolDeployer = await PancakeV3PoolDeployer.deploy()
+    pancakeV3PoolDeployer = await BeraV3PoolDeployer.deploy()
 
     pancakeV3PoolDeployer_address = pancakeV3PoolDeployer.address
     console.log('pancakeV3PoolDeployer', pancakeV3PoolDeployer_address)
   } else {
-    pancakeV3PoolDeployer = new ethers.Contract(
-      pancakeV3PoolDeployer_address,
-      artifacts.PancakeV3PoolDeployer.abi,
-      owner
-    )
+    pancakeV3PoolDeployer = new ethers.Contract(pancakeV3PoolDeployer_address, artifacts.BeraV3PoolDeployer.abi, owner)
   }
 
   let pancakeV3Factory_address = ''
   let pancakeV3Factory
   if (!pancakeV3Factory_address) {
-    const PancakeV3Factory = new ContractFactory(
-      artifacts.PancakeV3Factory.abi,
-      artifacts.PancakeV3Factory.bytecode,
-      owner
-    )
-    pancakeV3Factory = await PancakeV3Factory.deploy(pancakeV3PoolDeployer_address)
+    const BeraV3Factory = new ContractFactory(artifacts.BeraV3Factory.abi, artifacts.BeraV3Factory.bytecode, owner)
+    pancakeV3Factory = await BeraV3Factory.deploy(pancakeV3PoolDeployer_address)
 
     pancakeV3Factory_address = pancakeV3Factory.address
     console.log('pancakeV3Factory', pancakeV3Factory_address)
   } else {
-    pancakeV3Factory = new ethers.Contract(pancakeV3Factory_address, artifacts.PancakeV3Factory.abi, owner)
+    pancakeV3Factory = new ethers.Contract(pancakeV3Factory_address, artifacts.BeraV3Factory.abi, owner)
   }
 
   // Set FactoryAddress for pancakeV3PoolDeployer.
-  await pancakeV3PoolDeployer.setFactoryAddress(pancakeV3Factory_address);
-
+  await pancakeV3PoolDeployer.setFactoryAddress(pancakeV3Factory_address)
 
   const contracts = {
-    PancakeV3Factory: pancakeV3Factory_address,
-    PancakeV3PoolDeployer: pancakeV3PoolDeployer_address,
+    BeraV3Factory: pancakeV3Factory_address,
+    BeraV3PoolDeployer: pancakeV3PoolDeployer_address,
   }
 
   fs.writeFileSync(`./deployments/${networkName}.json`, JSON.stringify(contracts, null, 2))
